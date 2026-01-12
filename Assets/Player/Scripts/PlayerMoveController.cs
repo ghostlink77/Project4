31a3b70b178447ff35acf60df83f12baa3b998ad
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-01-13 04:45:37 +0900

    refactor: PlayerMove 스크립트 이름을 PlayerMoveController로 변경

---
+++ Assets/Player/Scripts/PlayerMoveController.cs
@@ -0,0 +1,23 @@
+/*
+이 스크립트는 플레이어 이동을 관리함
+플레이어 이동과 관련이 없는 스크립트는 이 코드에 작성하면 안된다.
+*/
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    public PlayerInputKey keyList;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    void 
+}
