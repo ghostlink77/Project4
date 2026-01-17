5431de6eb4bd3224b33b6974b1120093577f9144
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-01-18 03:04:31 +0900

    feat: 가장 가까운 적의 '방향' 구하는 기능 구현
    - 이전에는 가장 가까운 적 게임오브젝트를 반환하는 식으로 구현하고자 했지만, 이 게임에서 플레이어가 발사하는 투사체는 적에게 유도되는 투사체가 아닌 '적의 방향으로 뻗어나가는 투사체'이다
    - 따라서 단순히 투사체의 방향을 반환하는 식으로 구현하도록 한다
    - 이후에는 실제로 발사 속도에 맞춰 투사체를 발사하는 것을 구현할 예정이다

--- Assets/Weapon/Scripts/GetEnemyAngle.cs
+++ Assets/Weapon/Scripts/GetEnemyAngle.cs
@@ -1,6 +1,9 @@
 /*
 플레이어의 하위 오브젝트로 생성된 무기의 월드 좌표, 
 */
+using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GetEnemyAngle : MonoBehaviour
@@ -8,10 +11,13 @@ public class GetEnemyAngle : MonoBehaviour
     private Vector2 playerPos;
     private Vector2 targetPos;
     private GameObject targetObj;
+    private CircleCollider2D weaponCollider;
+    private List<GameObject> enemiesInRange = new List<GameObject>();
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        weaponCollider = GetComponent<CircleCollider2D>();
         GetPlayerPosition();
     }
 
@@ -20,13 +26,31 @@ public class GetEnemyAngle : MonoBehaviour
     {
         
     }
-    
+
+    // 트리거 콜라이더에 적이 들어올 때
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy")) enemiesInRange.Add(collision.gameObject);
+    }
+
+    // 트리거 콜라이더에서 적이 나갈 때
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            enemiesInRange.Remove(collision.gameObject);
+            if (targetObj == collision.gameObject) targetObj = null;
+        }
+    }
+
+    // 플레이어 위치 갱신하는 메서드
     void GetPlayerPosition()
     {
         playerPos = transform.position;
     }
     
-    void GetEnemyPosition()
+    // 적 위치 갱신하는 메서드
+    void GetTargetPosition()
     {
         // 타겟인 적의 게임 오브젝트 할당
         
@@ -34,10 +58,48 @@ public class GetEnemyAngle : MonoBehaviour
         targetPos = targetObj.transform.position;
     }
     
-    // 무기에서 총알이 나가야 하는 방향 계산
-    public Vector2 GetShootDirection()
+    // 매개변수로 시작 지점, 끝 지점 설정한 후 벡터 반환하는 메서드
+    public Vector2 GetDirectionVector(Vector2 startPos, Vector2 endPos)
     {
-        GetPlayerPosition(); GetEnemyPosition();
-        return targetPos - playerPos;
+        return endPos - startPos;
+    }
+    
+    // 제공된 2차원 벡터의 크기를 구하는 메서드
+    public double GetDistance(Vector2 directionVector)
+    {
+        double distance = Math.Pow(directionVector.x, 2) + Math.Pow(directionVector.y, 2);
+        return Math.Sqrt(distance);
+    }
+    
+    public Vector2 FindCLosestEnemyDirection()
+    {
+        int length = enemiesInRange.Count;
+        // 플레이어와 적의 거리 저장하는 리스트
+        List<double> enemyDistance = new List<double>();
+        
+        // 플레이어 위치 동기화
+        GetPlayerPosition();
+        
+        // 적과의 거리 저장하는 리스트에 거리들 작성
+        for (int i = 0; i < length; i++)
+        {
+            // 플레이어에서 적으로 도달하는 벡터 구하는 배열
+            enemyDistance.Add(GetDistance(GetDirectionVector(playerPos, enemiesInRange[i].transform.position)));
+        }
+        
+        int smallestEnemyIndex = 0;
+        double smallestDirection = enemyDistance[0];
+        
+        // 거리들 비교하고, 제일 작은 값과 인덱스 번호 찾아내기
+        for (int i = 1; i < length; i++)
+        {
+            if (smallestDirection > enemyDistance[i])
+            {
+                smallestEnemyIndex = i;
+                smallestDirection = enemyDistance[i];
+            }
+        }
+
+        return enemiesInRange[smallestEnemyIndex].transform.position;
     }
 }
---
+++ Assets/Weapon/Scripts/WeaponShootController.cs
@@ -0,0 +1,19 @@
+/*
+무기의 사격을 관리하는 스크립트
+*/
+using UnityEngine;
+
+public class WeaponShootController : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
