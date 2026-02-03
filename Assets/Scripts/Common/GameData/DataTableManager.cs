78a434a70a5d6f142ef322c6a4c5fbc5dcb77e1c
Bai Kyungmin <qorudalswl@naver.com>
2026-02-03 20:42:12 +0900

    Feat : Exp Data 불러오기 기능 구현

---
+++ Assets/Scripts/Common/GameData/DataTableManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DataTableManager : SingletonBehaviour<DataTableManager>
+{
+    public List<IGameData> GameDataList { get; private set; } = new();
+
+    
+    protected override void Init()
+    {
+        base.Init();
+
+        GameDataList.Add(new ExpData());
+    }
+
+    public T GetGameData<T>() where T : class, IGameData
+    {
+        return GameDataList.OfType<T>().FirstOrDefault();
+    }
+
+    public void SetData()
+    {
+        foreach (var data in GameDataList)
+        {
+            data.SetData();
+        }
+    }
+}
---
+++ Assets/Scripts/Common/GameData/ExpData.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ExpData : IGameData
+{
+    private Dictionary<float, float> experienceData = new Dictionary<float, float>();
+    private readonly string FILENAME = "ExpData";
+
+    public void SetData()
+    {
+        string path = Application.dataPath + "/Resources/DataTable/" + FILENAME + ".csv";
+
+        StreamReader reader = new StreamReader(path);
+        if (reader == null)
+        {
+            Debug.Log("ExpData.csv doesn't exist.");
+        }
+
+        reader.ReadLine();
+
+        bool isFinish = false;
+        while (isFinish == false)
+        {
+            string data = reader.ReadLine();
+            if (data == null)
+            {
+                isFinish = true;
+                break;
+            }
+
+            var splitData = data.Split(',');
+            if (float.TryParse(splitData[0], out float result1) && float.TryParse(splitData[1], out float result2))
+            {
+
+                experienceData.Add(result1, result2);
+            }
+            else
+            {
+                Debug.Log("LevelUpdata Error.");
+            }
+
+        }
+    }
+}
