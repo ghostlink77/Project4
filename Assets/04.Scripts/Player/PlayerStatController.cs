4f0ceb7e56e237401777ddfbb62f4a0da1dfc56f
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-02-09 02:28:15 +0900

    feat: 사망 후 부활까지 대기 시간 추가
    사망 후 부활까지 대기 시간을 float 변수화해서 기본값 1.5f로 설정함.
    그리고 프로퍼티로 설정하여, 값이 수정될 때마다 0 이하인지 점검하고, 0 이하라면 0을 대입하도록 설정함.
    또한 기존의 부활 과정에 대기시간을 적용하기 위해 부활을 새로운 코루틴 메서드로 관리하도록 설정함.
    기존의 플레이어 부활을 담당하던 메서드는 플레이어를 부활시킬 때 스탯 변화를 적용할 메서드로 변경됨.

--- Assets/04.Scripts/Player/PlayerStatController.cs
+++ Assets/04.Scripts/Player/PlayerStatController.cs
@@ -2,6 +2,7 @@
 플레이어 캐릭터의 게임 플레이 도중의 상태 변화, 성장 등을 관리하는 스크립트
 */
 using System;
+using System.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -45,6 +46,15 @@ public class PlayerStatController : MonoBehaviour, IDamageable
     public int Life {get => _life; set => _life = value;}
     private bool _dead = false;
     public bool Dead {get => _dead; set => _dead = value;}
+    
+    // 플레이어 부활까지 걸리는 시간이기 때문에 조정할 필요가 있음. 따라서 SerializeField를 적용함.
+    [SerializeField]
+    private float _reviveDelayTime = 1.5f;
+    public float ReviveDelayTime
+    {
+        get => _reviveDelayTime;
+        set => _reviveDelayTime = (value <= 0) ? 0 : value;
+    }
     private int _weaponSlotSize;
     public int WeaponSlotSize {get => _weaponSlotSize; set => _weaponSlotSize = value;}
     private int _passiveItemSlotSize;
@@ -148,17 +158,28 @@ public class PlayerStatController : MonoBehaviour, IDamageable
         // 플레이어 애니메이터 사망 트리거 실행
         _animator.SetBool("isDead", Dead);
         PlayerManager.Instance.PlayerMoveController.MoveVector = Vector2.zero;
-        PlayerRevive();
+        StartCoroutine(AfterReviveDelay());
     }
-    // 플레이어 부활시키는 메서드
-    private void PlayerRevive()
+    
+    private IEnumerator AfterReviveDelay()
+    {
+        Animator animator = PlayerManager.Instance.Animator;
+        animator.SetBool("isDead", true);
+        
+        yield return new WaitForSeconds(ReviveDelayTime);
+        PlayerReviveStatusSetting();
+    }
+
+    // 플레이어 부활시킬 때 스탯 변화를 적용할 메서드
+    private void PlayerReviveStatusSetting()
     {
         if (CheckRevivable() == true)
         {
             // 목숨 차감
             Life -= 1;
             Dead = false;
-            PlayerManager.Instance.Animator.SetBool("isDead", Dead);
+            // 사망 애니메이션 출력을 위한 설정
+            PlayerManager.Instance.Animator.SetBool("isDead", false);
             // 체력 최대 체력으로 초기화
             CurrentHp = MaxHp;
             Debug.Log($"부활 후 남은 목숨: {Life}, 플레이어 부활함");
