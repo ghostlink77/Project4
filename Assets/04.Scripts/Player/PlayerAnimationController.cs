2e5bec29e86993f968c0dc56193e68886793bc11
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-02-10 03:24:20 +0900

    refactor: 불필요한 유니티 함수를 삭제하고, 코드 주석 추가

--- Assets/04.Scripts/Player/PlayerAnimationController.cs
+++ Assets/04.Scripts/Player/PlayerAnimationController.cs
@@ -1,3 +1,7 @@
+/*
+플레이어의 애니메이션을 관리하는 스크립트.
+플레이어의 애니메이션 작동은 이곳에서 관리한다.
+*/
 using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
--- Assets/04.Scripts/Player/PlayerEventController.cs
+++ Assets/04.Scripts/Player/PlayerEventController.cs
@@ -1,16 +1,9 @@
+/*
+플레이어의 이벤트를 관리하는 스크립트
+이벤트를 사용해야 할 경우, 이 스크립트를 호출하여 사용하도록 한다.
+*/
 using UnityEngine;
 
 public class PlayerEventController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
