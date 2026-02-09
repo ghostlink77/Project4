bbdb34f0dfc9abba56b49efec6786dec9719cb2b
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-02-10 04:03:21 +0900

    feat: 이벤트 컨트롤러 스크립트에 상태 세 개 추가
    죽었을 때, 부활했을 때, 부활했을 때의 Action 이벤트를 생성함

--- Assets/04.Scripts/Player/PlayerEventController.cs
+++ Assets/04.Scripts/Player/PlayerEventController.cs
@@ -2,8 +2,10 @@
 플레이어의 이벤트를 관리하는 스크립트
 이벤트를 사용해야 할 경우, 이 스크립트를 호출하여 사용하도록 한다.
 */
+using System;
 using UnityEngine;
 
 public class PlayerEventController : MonoBehaviour
 {
+    public event Action Death, Revive, Hurt;
 }
