2c80b71a517b06dd76a5211972121d4d51247001
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-02-10 18:25:09 +0900

    feat: 주석 추가하고, 플레이어 움직임 관련 이벤트들 추가

--- Assets/04.Scripts/Player/PlayerEventController.cs
+++ Assets/04.Scripts/Player/PlayerEventController.cs
@@ -7,9 +7,16 @@ using UnityEngine;
 
 public class PlayerEventController : MonoBehaviour
 {
+    // 플레이어 상태 관련 이벤트
     public event Action Death, Revive, Hurt;
     
     public void CallDeath() => Death?.Invoke();
     public void CallRevive() => Revive?.Invoke();
     public void CallHurt() => Hurt?.Invoke();
+    
+    // 플레이어 이동 관련 이벤트
+    public event Action Move, Stop;
+    
+    public void CallMove() => Move?.Invoke();
+    public void CallStop() => Stop?.Invoke();
 }
