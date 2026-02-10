74a770a63edfafce6a95977bc64d1c49c4511f1d
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-02-10 22:24:28 +0900

    docs: 코드 구조화 지시문 추가함(PlayerEventControllers)

--- Assets/04.Scripts/Player/PlayerEventController.cs
+++ Assets/04.Scripts/Player/PlayerEventController.cs
@@ -7,16 +7,20 @@ using UnityEngine;
 
 public class PlayerEventController : MonoBehaviour
 {
+#region PlayerStatusEvents
     // 플레이어 상태 관련 이벤트
     public event Action Death, Revive, Hurt;
     
     public void CallDeath() => Death?.Invoke();
     public void CallRevive() => Revive?.Invoke();
     public void CallHurt() => Hurt?.Invoke();
-    
+#endregion
+
+#region PlayerMoveEvents
     // 플레이어 이동 관련 이벤트
     public event Action Move, Stop;
     
     public void CallMove() => Move?.Invoke();
     public void CallStop() => Stop?.Invoke();
+#endregion
 }
