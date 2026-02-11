3eae27d0bfdc6db76a887649314d77566abf9084
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-02-11 16:31:01 +0900

    feat: Hurt 이벤트에 추가하기 위한 메서드 생성

--- Assets/04.Scripts/Player/PlayerSoundController.cs
+++ Assets/04.Scripts/Player/PlayerSoundController.cs
@@ -4,12 +4,26 @@ public class PlayerSoundController : MonoBehaviour
 {
 #region 변수들
     private SoundManager _soundManager;
+    private PlayerManager _playerManager;
+    private PlayerEventController _playerEventController;
 #endregion
     
 #region SetUp 함수
     public void SetUp()
     {
         _soundManager = SoundManager.Instance;
+        _playerManager = PlayerManager.Instance;
+        _playerEventController = _playerManager.PlayerEventController;
+    }
+    
+    public void AddOnEvent()
+    {
+        _playerEventController.Hurt += OnEventHurt;
+    }
+    
+    private void OnEventHurt()
+    {
+        
     }
 #endregion
 }
