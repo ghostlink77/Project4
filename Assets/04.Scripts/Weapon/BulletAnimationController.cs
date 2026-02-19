0c37c651f2c73c53fed64f9e69871733618c7aea
Chillbok <45466997+Chillbok@users.noreply.github.com>
2026-02-19 09:22:03 +0900

    fix: 애니메이션이 처음부터 출력되지 않는 문제 해결
    애니메이션의 맨 앞 프레임부터 출력되도록 설정함

--- Assets/04.Scripts/Weapon/BulletAnimationController.cs
+++ Assets/04.Scripts/Weapon/BulletAnimationController.cs
@@ -13,6 +13,7 @@ public class BulletAnimationController : MonoBehaviour
     private BulletController _bulletController;
     private Collider2D _collider2D;
     private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
     private bool _deleteAfterAnimation;
     #endregion
     
@@ -41,6 +42,8 @@ public class BulletAnimationController : MonoBehaviour
         }
         _spriteRenderer.enabled = true;
         _collider2D.enabled = true;
+        
+        if(_animator != null) _animator.Play(_bulletAnimationClip.name, 0, 0f);
     }
     
     // 적 맞았을 때 실행될 코드
@@ -64,11 +67,11 @@ public class BulletAnimationController : MonoBehaviour
         if (!TryGetComponent<BulletController>(out _bulletController)) Debug.Log($"{nameof(_bulletController)}가 null임");
         if (!TryGetComponent<Collider2D>(out _collider2D)) Debug.Log($"{nameof(_collider2D)}가 null임");
         if (!TryGetComponent<SpriteRenderer>(out _spriteRenderer)) Debug.Log($"{nameof(_spriteRenderer)}가 null임");
+        if (!TryGetComponent<Animator>(out _animator)) Debug.Log($"{nameof(_animator)}가 null임");
+
         _deleteAfterAnimation = _bulletController.DeleteAfterAnimation;
         if (_deleteAfterAnimation && _bulletAnimationClip != null)
-        {
-            _bulletController.SetLifeTime(_bulletAnimationClip.length);
-        }
+        _bulletController.SetLifeTime(_bulletAnimationClip.length);
     }
 
     private void OnEnable()
