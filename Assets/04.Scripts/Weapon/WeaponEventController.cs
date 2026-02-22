c1db813c96fd1c9d3ddc6657f0dda48b68155560
chillbok <kylejunho@gmail.com>
2026-02-22 17:13:57 +0900

    refactor: 총알에 있던 이벤트를 WeaponEventController로 옮김

--- Assets/04.Scripts/Weapon/WeaponEventController.cs
+++ Assets/04.Scripts/Weapon/WeaponEventController.cs
@@ -5,8 +5,14 @@ public class WeaponEventController : MonoBehaviour
 {
     public event Action<WeaponStat> OnStatChanged;
     public event Action OnShoot;
+    
+    public event Action<GameObject> OnBulletGenerate, OnBulletHit, OnBulletDelete;
 
     public void CallOnStatChanged(WeaponStat stat) => OnStatChanged?.Invoke(stat);
     public void CallOnShoot() => OnShoot?.Invoke();
+    
+    public void CallOnBulletGenerate(GameObject bullet) => OnBulletGenerate?.Invoke(bullet);
+    public void CallOnBulletHit(GameObject bullet) => OnBulletHit?.Invoke(bullet);
+    public void CallOnBulletDelete(GameObject bullet) => OnBulletDelete?.Invoke(bullet);
 }
 
