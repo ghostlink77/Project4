1de89757a0e20ec0acf0c03361407355fd2da797
chillbok <kylejunho@gmail.com>
2026-03-01 09:36:18 +0900

    fix(#59): 무기 발사음이 제대로 출력되도록 수정
    무기 소리가 제대로 나지 않는 문제가 있었음.
    이전에 WeaponShootController를 수정하면서 OnShoot 이벤트를 호출하는 코드를 삭제했었던 것 같음.
    해당 코드를 다시 추가했음.

--- Assets/04.Scripts/Weapon/WeaponSoundController.cs
+++ Assets/04.Scripts/Weapon/WeaponSoundController.cs
@@ -10,6 +10,7 @@ public class WeaponSoundController : MonoBehaviour
 
     #region 스크립트 참조 변수
     private WeaponEventController _weaponEventController;
+    private SoundManager _soundManager;
     #endregion
 
     #region 컴포넌트 참조변수
@@ -36,35 +37,28 @@ public class WeaponSoundController : MonoBehaviour
         Debug.LogError($"{_audioSource.GetType()} null임");
         AddToEvent();
     }
-    
+
+    private void Start()
+    {
+        _soundManager = SoundManager.Instance;
+        if (_soundManager == null) Debug.LogError("사운드매니저가 씬에 없음");
+    }
+
     #region 이벤트 관련 변수
     private void AddToEvent()
     {
-        _weaponEventController.OnShoot += OnEventOnShoot;
+        _weaponEventController.OnShoot += PlayShootSound;
     }
     
     private void RemoveFromEvent()
     {
-        _weaponEventController.OnShoot -= OnEventOnShoot;
+        _weaponEventController.OnShoot -= PlayShootSound;
     }
     
-    private void OnEventOnShoot()
+    private void PlayShootSound()
     {
-        if (ObjectIsNull(_audioSource, "AudioSource")) return;
-        if (ObjectIsNull(_weaponShootSound, "Weapon Shoot Sound")) return;
-        _audioSource.PlayOneShot(_weaponShootSound);
-    }
-    #endregion
-    
-    #region 점검 스크립트
-    private bool ObjectIsNull(Object obj, string name)
-    {
-        if (obj == null)
-        {
-            Debug.LogWarning($"{name}이(가) null임");
-            return true;
-        }
-        return false;
+        _soundManager.PlaySFX(SoundType.Player, _weaponShootSound);
+        Debug.Log("무기 발사음 출력됨");
     }
     #endregion
 }
