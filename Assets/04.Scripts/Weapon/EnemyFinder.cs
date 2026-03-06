b749eaaa49b852a41dbfc786776327010285afa8
chillbok <kylejunho@gmail.com>
2026-03-06 12:34:21 +0900

    refactor: EnemyFinder에서 적 탐색 방식 수정함
    기존에는 TriggerEnter, TriggerExit 메서드를 기반으로 적을 탐색했었음.
    하지만 이 방식은 오브젝트에 콜라이더가 두 개 이상 있는 경우 사용할 수
    없는 방식이었음.
    따라서 좀 더 범용성 있게 활용하기 위해 EnemyFinder 탐색 방식을
    OverlapCircle 기반 즉시 탐색으로 변경했음.
    기존에는 계속 켜져 있는 콜라이더에 적이 들어올 때마다 인식했지만,
    이제는 호출할 때 딱 한 번만 주변의 적을 인식하므로 성능적으로도 개선이
    되었음.
    
    궁극적으로는 이 방식을 무기에서 적을 찾는 방식에도 도입할 수 있으리라고
    생각함.
    필요한 경우에 딱 한 번만 적을 인식하고 가장 가까운 적의 방향을 반환하기
    때문임.

--- Assets/04.Scripts/Weapon/EnemyFinder.cs
+++ Assets/04.Scripts/Weapon/EnemyFinder.cs
@@ -1,74 +1,47 @@
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Runtime.ExceptionServices;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemyFinder : MonoBehaviour
 {
-    private List<GameObject> _enemyDirectionList;
-    
     [Header("목표 적 레이어")]
     [SerializeField]
     private LayerMask _targetLayer;
-
-    public void SetList(List<GameObject> listToSaveEnemyDirection)
-    {
-        _enemyDirectionList = listToSaveEnemyDirection;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (((1 << collision.gameObject.layer) & _targetLayer) != 0)
-        {
-            _enemyDirectionList.Add(collision.gameObject);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (((1 << collision.gameObject.layer) & _targetLayer) != 0)
-        {
-            _enemyDirectionList.Remove(collision.gameObject);
-        }
-    }
     
-    public Vector2 GetClosestEnemy()
+    private Transform[] GetEnemiesInRange(float radius)
     {
-        int enemyCount = _enemyDirectionList.Count;
-        if (enemyCount == 0) return Vector2.zero;
+        Collider2D[] targetColliders = Physics2D.OverlapCircleAll(transform.position, radius, _targetLayer);
+        int targetCount = targetColliders.Length;
+        Transform[] detactedTargetsTransform = new Transform[targetCount];
+        
+        for (int i = 0; i < targetCount; i++)
+        detactedTargetsTransform[i] = targetColliders[i].transform;
 
-        Vector2 bulletPos = gameObject.transform.position;
-        Vector2 enemyPos = _enemyDirectionList[0].transform.position;
-        Vector2 direction = GetDirection(bulletPos, enemyPos);
-        float closestMultipleLength = GetMultipleLength(direction);
-        Vector2 closestDirection = direction;
+        return detactedTargetsTransform;
+    }
 
-        for (int i = 1; i < enemyCount; i++)
+    public Transform GetClosestEnemy(float radius)
+    {
+        Transform[] objects = GetEnemiesInRange(radius);
+        // 적을 찾을 수 없거나, 찾은 적이 없는 경우 null을 반환하도록 했음.
+        // 따라서 반환값이 null일 때는 적이 없다고 판단하도록 함.
+        if (objects == null || objects.Length == 0) return null;
+
+        Transform bestTarget = null;
+        float closestDistance = float.MaxValue;
+        Vector2 currentPosition = transform.position;
+
+        foreach (Transform currentObject in objects)
         {
-            enemyPos = _enemyDirectionList[i].transform.position;
-            direction = GetDirection(bulletPos, enemyPos);
-            float currentMultipleLength = GetMultipleLength(direction);
-            if (currentMultipleLength < closestMultipleLength)
+            if(currentObject == null) continue;
+
+            Vector2 differenceToTarget = (Vector2)currentObject.position - currentPosition;
+            float distanceToTarget = differenceToTarget.sqrMagnitude;
+            
+            if (distanceToTarget < closestDistance)
             {
-                closestDirection = direction;
-                closestMultipleLength = currentMultipleLength;
+                closestDistance = distanceToTarget;
+                bestTarget = currentObject;
             }
         }
-        return closestDirection.normalized;
-    }
-    
-    private Vector2 GetDirection(Vector2 start, Vector2 end)
-    {
-        return end - start;
-    }
-    private float GetMultipleLength(Vector2 direction)
-    {
-        return direction.x * direction.x + direction.y * direction.y;
-    }
-
-    void OnDisable()
-    {
-        _enemyDirectionList = null;
+        return bestTarget;
     }
 }
