1b170c0aa8923243e445483edc9561af313a622f
GongJaCh <81235791+ghostlink77@users.noreply.github.com>
2026-03-23 00:39:16 +0900

    Fix: 머지 충돌 문제 해결

--- Assets/04.Scripts/UI/InGame/InGameUIController.cs
+++ Assets/04.Scripts/UI/InGame/InGameUIController.cs
@@ -418,7 +418,7 @@ public class InGameUIController : MonoBehaviour
                 _itemSelectBtnDatas[index].ItemImage.preserveAspect = true;
             }
             if (_itemSelectBtnDatas[index].ItemLevelText != null) _itemSelectBtnDatas[index].ItemLevelText.text = $"Lv{ItemLevel-1} -> Lv{ItemLevel}";
-            if (_itemSelectBtnDatas[index].ItemDescriptionText != null) _itemSelectBtnDatas[index].ItemDescriptionText.text = newItemData.GetDescription(ItemLevel);
+            if (_itemSelectBtnDatas[index].ItemDescriptionText != null) _itemSelectBtnDatas[index].ItemDescriptionText.text = itemData.GetDescription(ItemLevel);
         }
 
         _itemSelectBtns[index].onClick.RemoveAllListeners();
